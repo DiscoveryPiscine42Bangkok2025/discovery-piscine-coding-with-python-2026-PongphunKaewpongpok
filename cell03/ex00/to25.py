e07c7acf5cd01928f5de85b556f2666a3b4680f4
PongphunKaewpongpok <138851758+PongphunKaewpongpok@users.noreply.github.com>
2026-02-11 00:16:41 +0700

    Update Cellule3.0

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,7 @@
+input_num = int(input("Enter a number less than 25\n"))
+
+if input_num > 25:
+    print("Error")
+else:
+    for i in range(26-input_num):
+        print("Inside the loop, my variable is", input_num+i)
