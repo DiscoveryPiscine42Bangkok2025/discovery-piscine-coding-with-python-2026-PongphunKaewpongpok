f735e4793d346f0c1590691a442afc5c890cb09c
PongphunKaewpongpok <138851758+PongphunKaewpongpok@users.noreply.github.com>
2026-02-10 21:13:51 +0700

    Update Cellule1.1 - 1.3

---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Pongphun"
+last_name = "Kaewpongpok"
+whole_name = first_name + " " + last_name
+
+print(whole_name)
---
+++ cell01/ex02/age.py
@@ -0,0 +1,5 @@
+my_real_age = 20
+constant_num = 42
+my_age = my_real_age + constant_num
+
+print(my_age)
