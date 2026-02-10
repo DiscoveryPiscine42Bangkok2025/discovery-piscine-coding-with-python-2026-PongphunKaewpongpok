f28a1441f5b09992ba3da740a8f6e568c602eb8c
PongphunKaewpongpok <138851758+PongphunKaewpongpok@users.noreply.github.com>
2026-02-11 01:50:45 +0700

    Update Cellule3.1 - 3.2

---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,4 @@
+input_num = int(input("Enter a number\n"))
+
+for i in range(10):
+    print(f"{i} x {input_num} =", i*input_num)
---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,4 @@
+str_input = input("What you gotta say? : ")
+
+while str_input != "STOP":
+    str_input = input("I got that! Anything else? : ")
