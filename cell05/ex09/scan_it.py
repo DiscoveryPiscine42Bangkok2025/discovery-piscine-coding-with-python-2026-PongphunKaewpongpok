59c01724b9bbb9416f8739557082550432939558
PongphunKaewpongpok <138851758+PongphunKaewpongpok@users.noreply.github.com>
2026-02-11 14:44:52 +0700

    Update Cellule5.9

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,13 @@
+import sys, re
+
+if len(sys.argv) != 3:
+    print("none")
+else:
+    str_keyword = sys.argv[1]
+    target_word = sys.argv[2]
+
+    word_count = len(re.findall(str_keyword, target_word))
+    if word_count == 0:
+        print("none")
+    else:
+        print(word_count)
