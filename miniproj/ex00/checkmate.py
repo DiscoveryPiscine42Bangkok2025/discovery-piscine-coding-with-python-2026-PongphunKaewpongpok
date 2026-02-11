2565b67c5a7666bc7d56fda34d082b99d4ff088b
PongphunKaewpongpok <138851758+PongphunKaewpongpok@users.noreply.github.com>
2026-02-12 01:32:56 +0700

    Update Mini-Project (ex00)

--- miniproj/ex00/checkmate.py
+++ miniproj/ex00/checkmate.py
@@ -1,5 +1,3 @@
-valid_character = {"K", "R", "P", "Q", "B"}
-
 def get_board_matrix(board):
     board_matrix = [[]]
     row = 0
@@ -30,20 +28,81 @@ def get_and_check_king(board_matrix, board_size):
         for column in range(board_size):
             if board_matrix[row][column] == "K":
                 king_count = king_count + 1
-                king_pos["x"] = column
-                king_pos["y"] = row
+                king_pos["column"] = column
+                king_pos["row"] = row
     if king_count == 1:
         return king_pos
-    else:
+    elif king_count > 1:
         print("There can only be one king.")
         return 0
+    elif king_count == 0:
+        print("There must be at least one king.")
+        return 0
+
+
+pieces_can_block = ["R", "P", "Q", "B"]
 
 def is_check(board_matrix, board_size, king_pos):
     for row in range(board_size):
         for column in range(board_size):
-            if board_matrix[row][column] not in valid_character:
+            if board_matrix[row][column] == "P":
+                 # Pawn can check at up-left and up-right of it position
+                if (((king_pos["column"] == column+1) or (king_pos["column"] == column-1)) and king_pos["row"] == row-1):
+                    print("Success")
+                    return
 
-    print("Success")
+            if board_matrix[row][column] == "R" or board_matrix[row][column] == "Q":
+                # Rook (and Queen) can check in same column or row
+                if king_pos["column"] == column or king_pos["row"] == row:
+                    # Check anything block it checking way?
+                    got_block = 0
+
+                    if king_pos["column"] == column:
+                        tem_r = row
+                        step_r = 1 if row < king_pos["row"] else -1
+
+                        while tem_r != king_pos["row"]:
+                            tem_r = tem_r + step_r
+                            if board_matrix[tem_r][column] in pieces_can_block:
+                                got_block = 1
+                                break
+                    else:
+                        tem_c = column
+                        step_c = 1 if column < king_pos["column"] else -1
+
+                        while tem_c != king_pos["column"]:
+                            tem_c = tem_c + step_c
+                            if board_matrix[tem_c][row] in pieces_can_block:
+                                got_block = 1
+                                break
+                    
+                    if not got_block:
+                        print("Success")
+                        return
+
+            if board_matrix[row][column] == "B" or board_matrix[row][column] == "Q":
+                # Bishop (and Queen) can check at same Diagonal of it position. if diagonal two position 
+                # If the positions are diagonals apart, when you subtract the two positions, columns and row will have the same value.
+                if (abs(king_pos["column"] - column) == abs(king_pos["row"] - row)):
+                    # Check anything block it checking way?
+                    tem_c = column
+                    tem_r = row
+                    step_c = 1 if column < king_pos["column"] else -1
+                    step_r = 1 if row < king_pos["row"] else -1
+                    
+                    got_block = 0
+                    while tem_c != king_pos["column"]:
+                        tem_c = tem_c + step_c
+                        tem_r = row + step_r
+                        if board_matrix[tem_r][tem_c] in pieces_can_block:
+                            got_block = 1
+                            break
+                    
+                    if not got_block:
+                        print("Success")
+                        return
+
+    print("Error")
 
 
 def checkmate(board):
@@ -61,4 +120,4 @@ def checkmate(board):
     
     is_check(board_matrix, board_size, king_pos)
     
-    print(board_matrix)
+    #print(board_matrix)
